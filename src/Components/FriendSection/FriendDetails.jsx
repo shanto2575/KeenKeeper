05e5a8995be4ce9882a81694c486337ec9121679
shanto2575 <shanto2575@gmail.com>
2026-04-15 19:25:57 +0600

    friend details status and tags added

--- src/Components/FriendSection/FriendDetails.jsx
+++ src/Components/FriendSection/FriendDetails.jsx
@@ -26,10 +26,21 @@ const FriendDetails = () => {
             <Link to={'/'}><button className='btn btn-error'>Go Back</button></Link>
         </div>
     }
-    const { name, picture, days_since_contact, email, bio, goal, next_due_date } = isExist;
+    const { name, picture, days_since_contact, email, status, tags, bio, goal, next_due_date } = isExist;
 
     const { handleCall, handleText, handleVideo } = useContext(FriendContext)
     // console.log(handleCall,called)
+    const style = (status) => {
+        if (status === 'overdue') {
+            return 'bg-red-500 text-white px-3 py-1 rounded-full  font-semibold'
+        }
+        if (status === 'almost due') {
+            return 'bg-orange-500 text-white px-3 py-1 rounded-full text-sm font-semibold'
+        }
+        if (status === 'on-track') {
+            return 'bg-purple-500 text-white px-3 py-1 rounded-full text-sm font-semibold'
+        }
+    }
 
     return (
         <div className="px-4">
@@ -39,6 +50,19 @@ const FriendDetails = () => {
                         <div className="flex flex-col items-center space-y-3">
                             <img src={picture} alt={name} className="rounded-full w-32 h-32 object-cover" />
                             <h2 className="text-2xl font-bold">{name}</h2>
+                            <span className={style(status)}>
+                                {status}
+                            </span>
+                            <div className='flex gap-2 flex-wrap justify-center'>
+                                {
+                                    tags.map((tag, index) => (
+                                        <span key={index} className='bg-green-200 px-2 py-1 rounded-full text-sm'>
+                                            {tag}
+                                        </span>
+                                    ))
+                                }
+                            </div>
+                            
                             <p className="text-gray-400">{bio}</p>
                             <p><span className="font-semibold">Email :</span> {email}</p>
                         </div>

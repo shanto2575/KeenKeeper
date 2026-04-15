407e412c69ac622a449ac7a0149190c9d3cf6627
shanto2575 <shanto2575@gmail.com>
2026-04-15 12:13:44 +0600

    rechart add to the stats pages

--- src/Context/FriendsProvider.jsx
+++ src/Context/FriendsProvider.jsx
@@ -11,10 +11,30 @@ const FriendsProvider = ({ children }) => {
         const isExistCall = called.find(calls => calls.id === currentCall.id)
 
         if (isExistCall) {
-            toast.error(`${currentCall.name} is alrady Timeline`)
+            // toast.error(`${currentCall.name} is alrady Timeline`)
+            toast.error(`${currentCall.name} is alrady Timeline`, {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+            });
         } else {
             setCalled([...called, currentCall])
-            toast.success(`${currentCall.name} is added to Timeline`)
+            // toast.success(`${currentCall.name} is added to Timeline`)
+            toast.success(`${currentCall.name} is added to Timeline`, {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+            });
         }
         // console.log(currentCall, called, 'call')
     }
@@ -22,20 +42,60 @@ const FriendsProvider = ({ children }) => {
         const isExistText = text.find(text => text.id === currentCall.id)
 
         if (isExistText) {
-            toast.error(`${currentCall.name} is alrady Timeline`)
+            // toast.error(`${currentCall.name} is alrady Timeline`)
+            toast.error(`${currentCall.name} is alrady Timeline`, {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+            });
         } else {
             setText([...text, currentCall])
-            toast.success(`${currentCall.name} is added to Timeline`)
+            // toast.success(`${currentCall.name} is added to Timeline`)
+            toast.success(`${currentCall.name} is added to Timeline`, {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+            });
         }
     }
     const handleVideo = (currentCall) => {
         const isExistVideo = video.find(video => video.id === currentCall.id)
 
         if (isExistVideo) {
-            toast.error(`${currentCall.name} is alrady Timeline`)
+            // toast.error(`${currentCall.name} is alrady Timeline`)
+            toast.error(`${currentCall.name} is alrady Timeline`, {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+            });
         } else {
             setVideo([...video, currentCall])
-            toast.success(`${currentCall.name} is added to Timeline`)
+            // toast.success(`${currentCall.name} is added to Timeline`)
+            toast.success(`${currentCall.name} is added to Timeline`, {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+            });
         }
     }
     const data = {
--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Legend, Pie, PieChart, Tooltip, ResponsiveContainer } from 'recharts';
+import { FriendContext } from '../Context/FriendsProvider';
 
 const Stats = () => {
-    return (
-        <div>
-            Stats
+  const { called, text, video } = useContext(FriendContext)
+
+  const data = [
+    { name: 'Text', value: text.length, fill: '#0088FE' },
+    { name: 'Call', value: called.length, fill: '#00C49F' },
+    { name: 'Video', value: video.length, fill: '#FFBB28' },
+  ];
+
+  return (
+    <div className='container mx-auto my-10 space-y-5'>
+      <h2 className='font-bold text-4xl'>Friendship Analytics</h2>
+      <div className='shadow-[0_0_5px_rgba(0,0,0,0.15)] rounded-2xl p-5'>
+        <h2 className='text-2xl font-semibold'>By Interaction Type</h2>
+        <div className="w-full h-[400px] min-w-0">
+          <ResponsiveContainer width="100%" height="100%">
+            <PieChart>
+              <Pie
+                data={data}
+                dataKey="value"
+                innerRadius={90}
+                outerRadius={150}
+                paddingAngle={5}
+                cornerRadius={8}
+              />
+              <Tooltip />
+              <Legend />
+            </PieChart>
+          </ResponsiveContainer>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Stats
